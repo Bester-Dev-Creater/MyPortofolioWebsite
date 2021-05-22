dd5030488d90a48d0a033d4a4c5f7056ca5f1545
Bester, Charl <bestercharl1999@gmail.com>
2021-05-22 23:09:56 +0200

    fixed mobile nav bar and made scroll to top faster

--- src/app/components/scroll-top/scroll-top.component.ts
+++ src/app/components/scroll-top/scroll-top.component.ts
@@ -30,7 +30,7 @@ export class ScrollTopComponent implements OnInit {
 
       if (currentScroll > 0) {
         window.requestAnimationFrame(smoothscroll);
-        window.scrollTo(0, currentScroll - (currentScroll / 8));
+        window.scrollTo(0, currentScroll - (currentScroll / 5));
       }
 
     })();
